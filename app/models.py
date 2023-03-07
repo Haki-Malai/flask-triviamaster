a39030f4e9ae8559ec36fca9a2c3c4d942645827
Haki-Malai <p17mala@ionio.gr>
2023-03-07 22:02:43 +0200

    Add Question and Answer models
    The Question model now has a one-to-many relationship with the Answer model.
    
    The Answer model has a body, a boolean indicating whether it is the correct
    answer, and a foreign key to the Question model.

--- app/models.py
+++ app/models.py
@@ -2,21 +2,13 @@ from app import db
 
 
 class Question(db.Model):
-    __tablename__ = 'questions'
-
     id = db.Column(db.Integer, primary_key=True)
-    question = db.Column(db.String(255), nullable=False)
-    answer = db.Column(db.String(255), nullable=False)
+    body = db.Column(db.String(255), nullable=False)
+    type = db.Column(db.String(128), nullable=False)
+    category = db.Column(db.String(128), nullable=False)
     difficulty = db.Column(db.Integer, nullable=False)
-    category = db.Column(db.String(255), nullable=False)
-    rating = db.Column(db.Integer, nullable=False)
 
-    def __init__(self, question, answer, difficulty, category, rating):
-        self.question = question
-        self.answer = answer
-        self.difficulty = difficulty
-        self.category = category
-        self.rating = rating
+    answers = db.relationship('Answer', back_populates='question')
 
     def __repr__(self):
         return "<Question %r>" % self.question
@@ -25,9 +17,18 @@ class Question(db.Model):
         db.session.add(self)
         db.session.commit()
 
-    def update(self):
-        db.session.commit()
-
     def delete(self):
         db.session.delete(self)
         db.session.commit()
+
+
+class Answer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(128), nullable=False)
+    correct = db.Column(db.Boolean, nullable=False)
+    question_id = db.Column(db.Integer, db.ForeignKey('question.id'), nullable=False)
+
+    question = db.relationship('Question', back_populates='answers')
+
+    def __repr__(self):
+        return "<Answer %r>" % self.answer
