b00420caed67478a43aa81010f22faa752e960aa
Haki-Malai <p17mala@ionio.gr>
2023-03-07 22:45:40 +0200

    Add CLI command to initialize the database
    This command fetches questions from Open Trivia DB API and inserts them into the database.
    
    The command accepts two optional arguments:
    
    - amount: Number of questions to fetch. Max 50.
    - drop: Drop all tables before initializing.

--- app/cli.py
+++ app/cli.py
@@ -1,9 +1,62 @@
 import click
 from flask import Blueprint
 
+from app import db
+
 bp = Blueprint('cli', __name__)
 
 
 @bp.cli.command('init')
-def init():
+@click.option('--amount', default=10, help='Number of questions to fetch. Max 50.')
+@click.option('--drop', is_flag=True, help='Drop all tables before initializing.')
+def init(amount, drop):
+    """Initialize the database.
+    """
+    import requests
+    from app.models import Question, Answer, Category
+
+    # Drop all tables if drop flag is set
+    if drop:
+        db.drop_all()
+        db.create_all()
+
+    # Fetch questions from Open Trivia DB API
+    response = requests.get(f'https://opentdb.com/api.php?amount={amount}')
+    data = response.json()["results"]
+    difficulty = {'easy': 1, 'medium': 2, 'hard': 3}
+
+    for item in data:
+        # Create category if not exists
+        if not Category.query.filter_by(name=item['category']).first():
+            category = Category(name=item['category'])
+            db.session.add(category)
+            db.session.commit()
+
+        question = Question(
+            body=item['question'],
+            type=int(item['type'] == 'boolean'),
+            difficulty=difficulty[item['difficulty']],
+            category_id=Category.query.filter_by(name=item['category']).first().id
+        )
+        db.session.add(question)
+        db.session.commit()
+
+        # Create incorrect answers
+        for i in range(len(item['incorrect_answers'])):
+            incorect_answer = Answer(
+                body=item['incorrect_answers'][i],
+                correct=False,
+                question_id=question.id
+            )
+            db.session.add(incorect_answer)
+
+        # Create correct answer
+        correct_answer = Answer(
+            body=item['correct_answer'],
+            correct=True,
+            question_id=question.id
+        )
+        db.session.add(correct_answer)
+        db.session.commit()
+
     click.echo('Initialized the database.')
--- app/models.py
+++ app/models.py
@@ -5,21 +5,14 @@ class Question(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(255), nullable=False)
     type = db.Column(db.String(128), nullable=False)
-    category = db.Column(db.String(128), nullable=False)
     difficulty = db.Column(db.Integer, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
 
     answers = db.relationship('Answer', back_populates='question')
+    category = db.relationship('Category', back_populates='questions')
 
     def __repr__(self):
-        return "<Question %r>" % self.question
-
-    def insert(self):
-        db.session.add(self)
-        db.session.commit()
-
-    def delete(self):
-        db.session.delete(self)
-        db.session.commit()
+        return "<Question %r>" % self.body
 
 
 class Answer(db.Model):
@@ -31,4 +24,14 @@ class Answer(db.Model):
     question = db.relationship('Question', back_populates='answers')
 
     def __repr__(self):
-        return "<Answer %r>" % self.answer
+        return "<Answer %r>" % self.body
+
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), nullable=False)
+
+    questions = db.relationship('Question', back_populates='category')
+
+    def __repr__(self):
+        return "<Category %r>" % self.name
