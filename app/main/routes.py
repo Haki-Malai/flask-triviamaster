9767ff47b34ca15bc9c776713e77a8c4c6781556
Haki-Malai <p17mala@ionio.gr>
2023-03-10 19:33:54 +0200

    Add game functionality
    This commit adds the ability to start a game and answer questions.
    
    The game is started by clicking the "Play on all categories" button on the
    home page. The user is then redirected to the game page where they can answer
    questions.
    
    The game page shows the user's progress through the game and the current
    question. The user can answer the question by selecting one of the answers.
    
    The game page also shows the user's score.

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,8 +1,8 @@
-from flask import render_template, current_app
+from flask import render_template, current_app, request, redirect, url_for
 
 from app import db
 from app.main import bp
-from app.models import Category, Question
+from app.models import Category, Question, Game
 
 
 @bp.route('/')
@@ -11,3 +11,32 @@ def index():
         'index.html',
         config=current_app.config,
         categories=Category.query.order_by(Category.name).all())
+
+
+@bp.route('/game')
+def start_game():
+    game = Game()
+    db.session.add(game)
+    game.generate_questions()
+    db.session.commit()
+    return redirect(url_for('main.show_game', game_id=game.id))
+
+
+@bp.route('/game/<int:game_id>')
+def show_game(game_id):
+    return render_template(
+        'game.html',
+        config=current_app.config,
+        game=Game.query.get(game_id),
+        question=Game.query.get(game_id).next_question(),
+        categories=Category.query.order_by(Category.name).all())
+
+
+@bp.route('/game/<int:game_id>', methods=['POST'])
+def play_game(game_id):
+    game = Game.query.get(game_id)
+    question_id = request.form.get('question_id')
+    answer_id = request.form.get('answer_id')
+    game.answer_question(question_id, answer_id)
+    db.session.commit()
+    return redirect(url_for('main.play_game', game_id=game.id))
--- app/models.py
+++ app/models.py
@@ -1,4 +1,50 @@
+from sqlalchemy.sql.expression import func
+
 from app import db
+
+
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    score = db.Column(db.Integer, default=0, nullable=False)
+    game_questions = db.relationship('GameQuestion',
+                                     back_populates='game',
+                                     lazy='dynamic')
+
+    def __repr__(self):
+        return "<Game %r>" % self.id
+
+    @property
+    def questions(self):
+        return [game_question.question for game_question in self.game_questions]
+    
+    @property
+    def question_count(self):
+        return self.game_questions.count()
+    
+    @property
+    def answered_count(self):
+        return self.game_questions.filter_by(answered=True).count()
+    
+    @property
+    def progress(self):
+        return self.answered_count / self.question_count * 100
+    
+    def next_question(self):
+        for game_question in self.game_questions:
+            if not game_question.answered:
+                return game_question.question
+
+    def generate_questions(self, num_questions=5, category_id=None):
+        if category_id:
+            query = Category.query.get(category_id).questions
+        else:
+            query = Question.query
+        questions = query.order_by(func.random()).limit(num_questions).all()
+        
+        for question in questions:
+            game_questions = GameQuestion(game_id=self.id,
+                                          question_id=question.id)
+            db.session.add(game_questions)
 
 
 class Question(db.Model):
@@ -6,20 +52,41 @@ class Question(db.Model):
     body = db.Column(db.String(255), nullable=False)
     type = db.Column(db.String(128), nullable=False)
     difficulty = db.Column(db.Integer, nullable=False)
-    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    category_id = db.Column(db.Integer,
+                            db.ForeignKey('category.id'),
+                            nullable=False)
 
     answers = db.relationship('Answer', back_populates='question')
     category = db.relationship('Category', back_populates='questions')
+    game_questions = db.relationship('GameQuestion',
+                                     back_populates='question')
 
     def __repr__(self):
         return "<Question %r>" % self.body
+
+
+class GameQuestion(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    answered = db.Column(db.Boolean, default=False, nullable=False)
+    game_id = db.Column(db.Integer, db.ForeignKey('game.id'), nullable=False)
+    question_id = db.Column(db.Integer,
+                            db.ForeignKey('question.id'),
+                            nullable=False)
+
+    game = db.relationship('Game', back_populates='game_questions')
+    question = db.relationship('Question', back_populates='game_questions')
+
+    def __repr__(self):
+        return "<GameQuestion %r>" % self.id
 
 
 class Answer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(128), nullable=False)
     correct = db.Column(db.Boolean, nullable=False)
-    question_id = db.Column(db.Integer, db.ForeignKey('question.id'), nullable=False)
+    question_id = db.Column(db.Integer,
+                            db.ForeignKey('question.id'),
+                            nullable=False)
 
     question = db.relationship('Question', back_populates='answers')
 
@@ -34,4 +101,4 @@ class Category(db.Model):
     questions = db.relationship('Question', back_populates='category')
 
     def __repr__(self):
-        return "<Category %r>" % self.name
+            return "<Category %r>" % self.name
