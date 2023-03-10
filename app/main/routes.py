d0543a6af44b95c4be1212cfd4f237b769280b74
Haki-Malai <p17mala@ionio.gr>
2023-03-10 17:13:11 +0200

    Add categories to the navbar
    The categories are now displayed in the navbar.
    
    The navbar is now dark.
    
    The progress bar is now dark.
    
    The radio buttons are now dark.
    
    The submit button is now dark.

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,11 +1,13 @@
-from flask import render_template
+from flask import render_template, current_app
 
 from app import db
 from app.main import bp
-from app.models import Question
+from app.models import Category, Question
 
 
 @bp.route('/')
 def index():
     return render_template('index.html',
-                           question=Question.query.first())
+                           question=Question.query.first(),
+                           config=current_app.config,
+                           categories=Category.query.order_by(Category.name).all())
